d4fd5dd37964f8eb867cacf29b75c6e257cbffbc
Siddhesh Murlidhar Yadav <92988145+Siddhesh-Yadav@users.noreply.github.com>
2022-04-09 23:53:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // Declaring variables
-score = 0;
-bgmusic = new Audio("sounds/bgmusic.mp3");
-protonshot = new Audio("sounds/shoot.mp3");
-blast = new Audio("sounds/blast.mp3");
-gameovers = new Audio("sounds/gameover.wav");
-highScore = document.getElementById("highScore");
-var body = document.getElementsByTagName("body")[0];
-var deviceWidth = window.innerWidth;
-var distance = deviceWidth/7.5;
-var deviceHeight = window.innerHeight;
-var gameOver = document.querySelector(".gameOver");
-var canon = document.getElementById("canon");
-var defenderShip = document.getElementById("defenderShip");
-var invaders = document.querySelectorAll(".invader")
-var invader1 = document.querySelector("#invader1");
-var invader2 = document.querySelector("#invader2");
-var invader3 = document.querySelector("#invader3");
+let score = 0;
 let x = 0;
+const bgmusic = new Audio("sounds/bgmusic.mp3");
+const protonshot = new Audio("sounds/shoot.mp3");
+const blast = new Audio("sounds/blast.mp3");
+const gameovers = new Audio("sounds/gameover.wav");
+const deviceWidth = window.innerWidth;
+const distance = deviceWidth/7.5;
+const deviceHeight = window.innerHeight;
+highScore = document.getElementById("highScore");
+const body = document.getElementsByTagName("body")[0];
+const gameOver = document.querySelector(".gameOver");
+const canon = document.getElementById("canon");
+const defenderShip = document.getElementById("defenderShip");
+const invaders = document.querySelectorAll(".invader")
+const invader1= document.querySelector("#invader1");
+const invader2 = document.querySelector("#invader2");
+const invader3 = document.querySelector("#invader3");
 
 setTimeout(() => {
   bgmusic.play();
@@ -50,9 +50,7 @@ function shoot() {
 }
 
 function left() {
-  var leftX = parseInt(
-    window.getComputedStyle(defenderShip, null).getPropertyValue("left")
-  );
+  var leftX = parseInt(window.getComputedStyle(defenderShip, null).getPropertyValue("left"));
   if (x > -3) {
     x = x - 1;
     defenderShip.style.left = leftX - distance + "px";
@@ -60,9 +58,7 @@ function left() {
 }
 
 function right() {
-  var leftX = parseInt(
-    window.getComputedStyle(defenderShip, null).getPropertyValue("left")
-  );
+  var leftX = parseInt(window.getComputedStyle(defenderShip, null).getPropertyValue("left"));
   if (x < 3){
     x = x + 1;
     defenderShip.style.left = leftX + distance + "px";
@@ -71,106 +67,58 @@ function right() {
 
 // Function for reducing animation duration 
 function reduceDur(){
-  aniDur = parseFloat(
-    window
-      .getComputedStyle(invader1, null)
-      .getPropertyValue("animation-duration")
-  );
+  aniDur = parseFloat(window.getComputedStyle(invader1, null).getPropertyValue("animation-duration"));
   newDur = aniDur - 0.3;
   invaders.forEach(element => {element.style.animationDuration = newDur + "s"});
 }
 
+
+const getPoints = (ship)=>{
+    blast.play();
+    setTimeout(() => {
+      blast.pause();
+      blast.currentTime = 0;
+    }, 1000);
+    score += 1;
+    updateScore(score);
+    ship.src = "images/blast.jpg";
+    canon.style.display = "none";
+    reduceDur();
+    setTimeout(() => {
+      ship.classList.remove("invaderAni");
+      ship.style.visibility = "hidden";
+    }, 50);
+    setTimeout(() => {
+      ship.src = "images/invader.png";
+      ship.style.visibility = "visible";
+      ship.classList.add("invaderAni");
+      canon.style.display = "inline";
+    }, 500);
+}
 setInterval(() => {
   // Getting positions of invader ship
-  dy1 = parseInt(
-    window.getComputedStyle(invader1, null).getPropertyValue("bottom")
-  );
-  dy2 = parseInt(
-    window.getComputedStyle(invader2, null).getPropertyValue("bottom")
-  );
-  dy3 = parseInt(
-    window.getComputedStyle(invader3, null).getPropertyValue("bottom")
-  );
-  dy = parseInt(
-    window.getComputedStyle(canon, null).getPropertyValue("bottom")
-  );
+  dy1 = parseInt(window.getComputedStyle(invader1, null).getPropertyValue("bottom"));
+  dy2 = parseInt(window.getComputedStyle(invader2, null).getPropertyValue("bottom"));
+  dy3 = parseInt(window.getComputedStyle(invader3, null).getPropertyValue("bottom"));
+  dy = parseInt(window.getComputedStyle(canon, null).getPropertyValue("bottom"));
 
   // Getting the differnece
   offsetX1 = Math.abs(dy1 - dy);
   offsetX2 = Math.abs(dy2 - dy);
-  offsetX3 = Math.abs(dy3 - dy);
-  
-  // writing logic for invader1 ship
+  offsetX3 = Math.abs(dy3 - dy);  
+  // writing logic for ship ship
   if (offsetX1 > 600  && x == -3) {
-    blast.play();
-    setTimeout(() => {
-      blast.pause();
-      blast.currentTime = 0;
-    }, 1000);
-    score += 1;
-    updateScore(score);
-    invader1.src = "images/blast.jpg";
-    canon.style.display = "none";
-    reduceDur();
-    setTimeout(() => {
-      invader1.classList.remove("invaderAni");
-      invader1.style.visibility = "hidden";
-    }, 50);
-    setTimeout(() => {
-      invader1.src = "images/invader.png";
-      invader1.style.visibility = "visible";
-      invader1.classList.add("invaderAni");
-      canon.style.display = "inline";
-    }, 500);
-  }
-  
+    getPoints(invader1)
+  }  
   // writing logic for invader2 ship
   if (offsetX2 > 600 && x == 0) {
-    blast.play();
-    setTimeout(() => {
-      blast.pause();
-      blast.currentTime = 0;
-    }, 1000);
-    score += 1;
-    updateScore(score);
-    invader2.src = "images/blast.jpg";
-    canon.style.display = "none";
-    reduceDur();
-    setTimeout(() => {
-      invader2.classList.remove("invaderAni");
-      invader2.style.visibility = "hidden";
-    }, 50);
-    setTimeout(() => {
-      invader2.src = "images/invader.png";
-      invader2.style.visibility = "visible";
-      invader2.classList.add("invaderAni");
-      canon.style.display = "inline";
-    }, 500);
+    getPoints(invader2);
   }
-  
   // writing logic for invader3 ship
   if (offsetX3 > 600  && x == 3) {
-    blast.play();
-    setTimeout(() => {
-      blast.pause();
-      blast.currentTime = 0;
-    }, 1000);
-    score += 1;
-    updateScore(score);
-    invader3.src = "images/blast.jpg";
-    canon.style.display = "none";
-    reduceDur();
-    setTimeout(() => {
-      invader3.classList.remove("invaderAni");
-      invader3.style.visibility = "hidden";
-    }, 50);
-    setTimeout(() => {
-      invader3.src = "images/invader.png";
-      invader3.style.visibility = "visible";
-      invader3.classList.add("invaderAni");
-      canon.style.display = "inline";
-    }, 500);
+    getPoints(invader3)
   }
+  
 }, 20);
 
 // for updating score
@@ -180,21 +128,15 @@ function updateScore(score) {
 
 // setting highscore
 storedValue = localStorage.getItem("highScore");
-if (storedValue > 0) {
+if (storedValue === null) {
+  localStorage.setItem("highScore", 0);
+}else{
   highScore.innerHTML = "Highscore:" + storedValue;
 }
-
-// Gameover logic
 setInterval(() => {
-  dox1 = parseInt(
-    window.getComputedStyle(invader1, null).getPropertyValue("top")
-  );
-  dox2 = parseInt(
-    window.getComputedStyle(invader2, null).getPropertyValue("top")
-  );
-  dox3 = parseInt(
-    window.getComputedStyle(invader3, null).getPropertyValue("top")
-  );
+  dox1 = parseInt(window.getComputedStyle(invader1, null).getPropertyValue("top"));
+  dox2 = parseInt(window.getComputedStyle(invader2, null).getPropertyValue("top"));
+  dox3 = parseInt(window.getComputedStyle(invader3, null).getPropertyValue("top"));
   if (dox1 > innerHeight - 70 || dox2 > innerHeight - 70 || dox3 > innerHeight - 70) {
     j = 0;
     j = storedValue;
@@ -211,5 +153,4 @@ setInterval(() => {
     }
     localStorage.setItem("highScore", j);
   }
-}, 10);
-
+}, 100);
